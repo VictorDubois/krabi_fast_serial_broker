5e937a8d5eed5674382df6b9185a16cc2b1aa79d
Victor Dubois <victor.dubois@navya.tech>
2025-09-06 17:32:22 +0200

    clang format

--- src/simple_stm32_broker.cpp
+++ src/simple_stm32_broker.cpp
@@ -1,43 +1,52 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <chrono>
-#include <thread>
-#include <iomanip>
-#include <fcntl.h>
-#include <errno.h>
-#include <termios.h>
-#include <unistd.h>
-#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "krabi_msgs/msg/encoders.hpp"
 #include "krabi_msgs/msg/motors_cmd.hpp"
 #include "krabi_msgs/msg/motors_parameters.hpp"
 #include "krabi_msgs/msg/odom_lighter.hpp"
-#include "std_msgs/msg/bool.hpp"
 #include "nav_msgs/msg/odometry.hpp"
-#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include <bitset>
-
+#include <chrono>
+#include <errno.h>
+#include <fcntl.h>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <termios.h>
+#include <thread>
+#include <unistd.h>
+#include <vector>
 
 using namespace std::chrono_literals;
 
 class SimpleBrokerSTM32 : public rclcpp::Node
 {
 public:
-    SimpleBrokerSTM32() : Node("simple_broker_stm32")
+    SimpleBrokerSTM32()
+      : Node("simple_broker_stm32")
     {
         subscription_cmd_vel_ = this->create_subscription<geometry_msgs::msg::Twist>(
-            "cmd_vel", 10, std::bind(&SimpleBrokerSTM32::callback_cmd_vel, this, std::placeholders::_1));
+          "cmd_vel",
+          10,
+          std::bind(&SimpleBrokerSTM32::callback_cmd_vel, this, std::placeholders::_1));
 
         subscription_motors_cmd_ = this->create_subscription<krabi_msgs::msg::MotorsCmd>(
-            "motors_cmd", 10, std::bind(&SimpleBrokerSTM32::callback_motors_cmd, this, std::placeholders::_1));
+          "motors_cmd",
+          10,
+          std::bind(&SimpleBrokerSTM32::callback_motors_cmd, this, std::placeholders::_1));
 
-        subscription_motors_parameters_ = this->create_subscription<krabi_msgs::msg::MotorsParameters>(
-            "motors_parameters", 10, std::bind(&SimpleBrokerSTM32::callback_motors_parameters, this, std::placeholders::_1));
+        subscription_motors_parameters_
+          = this->create_subscription<krabi_msgs::msg::MotorsParameters>(
+            "motors_parameters",
+            10,
+            std::bind(&SimpleBrokerSTM32::callback_motors_parameters, this, std::placeholders::_1));
 
         subscription_enable_motor_ = this->create_subscription<std_msgs::msg::Bool>(
-            "enable_motor", 10, std::bind(&SimpleBrokerSTM32::callback_enable_motors, this, std::placeholders::_1));
+          "enable_motor",
+          10,
+          std::bind(&SimpleBrokerSTM32::callback_enable_motors, this, std::placeholders::_1));
 
         odom_pub_ = this->create_publisher<krabi_msgs::msg::OdomLighter>("odom_lighter", 10);
         encoders_pub_ = this->create_publisher<krabi_msgs::msg::Encoders>("encoders", 10);
@@ -45,7 +54,7 @@ public:
         timer_ = this->create_wall_timer(1ms, std::bind(&SimpleBrokerSTM32::read_serial, this));
 
         serial_fd_ = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
-        //serial_fd_ = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
+        // serial_fd_ = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
         if (serial_fd_ == -1)
         {
             RCLCPP_ERROR(this->get_logger(), "open_port: Unable to open /dev/ttyACM0 - ");
@@ -67,26 +76,31 @@ public:
     }
 
 private:
-
-    // Define CRC parameters
-    #define CRC_POLYNOMIAL 0x1021
-    #define CRC_INITIAL_VALUE 0xFFFF
+// Define CRC parameters
+#define CRC_POLYNOMIAL 0x1021
+#define CRC_INITIAL_VALUE 0xFFFF
 
     // Function to calculate CRC
-    uint16_t calculate_crc(const uint8_t *data, int length) {
+    uint16_t calculate_crc(const uint8_t* data, int length)
+    {
         uint16_t crc = CRC_INITIAL_VALUE;
-        
-        for (int i = 0; i < length; i++) {
+
+        for (int i = 0; i < length; i++)
+        {
             crc ^= data[i] << 8;
-            for (int j = 0; j < 8; j++) {
-                if (crc & 0x8000) {
+            for (int j = 0; j < 8; j++)
+            {
+                if (crc & 0x8000)
+                {
                     crc = (crc << 1) ^ CRC_POLYNOMIAL;
-                } else {
+                }
+                else
+                {
                     crc <<= 1;
                 }
             }
         }
-        
+
         return crc;
     }
 
@@ -117,8 +131,8 @@ private:
         options.c_oflag &= ~OPOST;
 
         // Set terminal to canonical mode
-        //options.c_lflag |= (ICANON | ECHO | ECHOE | ISIG);
-        //options.c_oflag |= OPOST;
+        // options.c_lflag |= (ICANON | ECHO | ECHOE | ISIG);
+        // options.c_oflag |= OPOST;
 
         // Set minimum characters to read
         options.c_cc[VMIN] = 0;
@@ -144,7 +158,8 @@ private:
         float_array_to_send.push_back(msg->max_current_right);
         float_array_to_send.push_back(msg->max_current);
 
-        //RCLCPP_WARN_STREAM(this->get_logger(),  float_array_to_send[0] << ", " << float_array_to_send[1] << ", " << float_array_to_send[2]);
+        // RCLCPP_WARN_STREAM(this->get_logger(),  float_array_to_send[0] << ", " <<
+        // float_array_to_send[1] << ", " << float_array_to_send[2]);
 
         write_float_to_serial('p', float_array_to_send);
     }
@@ -157,8 +172,9 @@ private:
         float_array_to_send.push_back(msg->pwm_override_left);
         float_array_to_send.push_back(msg->pwm_override_right);
         float_array_to_send.push_back(msg->reset_encoders);
-        //RCLCPP_WARN_STREAM(this->get_logger(),  float_array_to_send[0] << ", " << float_array_to_send[1] << ", " << float_array_to_send[2]);
- 
+        // RCLCPP_WARN_STREAM(this->get_logger(),  float_array_to_send[0] << ", " <<
+        // float_array_to_send[1] << ", " << float_array_to_send[2]);
+
         write_float_to_serial('c', float_array_to_send);
     }
 
@@ -167,7 +183,8 @@ private:
         std::vector<float> float_array_to_send;
         float_array_to_send.push_back(msg->linear.x);
         float_array_to_send.push_back(msg->angular.z);
-        //RCLCPP_WARN_STREAM(this->get_logger(),  float_array_to_send[0] << ", " << float_array_to_send[1]);
+        // RCLCPP_WARN_STREAM(this->get_logger(),  float_array_to_send[0] << ", " <<
+        // float_array_to_send[1]);
 
         write_float_to_serial('v', float_array_to_send);
     }
@@ -179,15 +196,17 @@ private:
         sprintf((char*)(a_out + start_pos), "%08X", floatHex); // Convert to hexadecimal string
     }
 
-    void write_float_to_serial(char channel_name, const std::vector<float> &float_values)
+    void write_float_to_serial(char channel_name, const std::vector<float>& float_values)
     {
-        uint8_t hex_values[10 * 8] = {0}; // Initialize to 0
+        uint8_t hex_values[10 * 8] = { 0 }; // Initialize to 0
         for (size_t i = 0; i < float_values.size(); ++i)
         {
             float_to_hex(float_values[i], hex_values, i * 8);
         }
         // Send data over serial
-        std::string message = std::string(1, channel_name) + std::string(reinterpret_cast<char*>(hex_values), sizeof(hex_values)) + "\n";
+        std::string message = std::string(1, channel_name)
+                              + std::string(reinterpret_cast<char*>(hex_values), sizeof(hex_values))
+                              + "\n";
         ssize_t n = write(serial_fd_, message.c_str(), message.length());
         if (n < 0)
         {
@@ -195,22 +214,25 @@ private:
         }
         else
         {
-            //RCLCPP_WARN(this->get_logger(), "Sending: %s", message.c_str());
-            //std::cout << "Sending: " << channel_name << std::string(reinterpret_cast<char*>(hex_values), sizeof(hex_values)) << std::endl;
-            //std::cout.flush();
+            // RCLCPP_WARN(this->get_logger(), "Sending: %s", message.c_str());
+            // std::cout << "Sending: " << channel_name <<
+            // std::string(reinterpret_cast<char*>(hex_values), sizeof(hex_values)) << std::endl;
+            // std::cout.flush();
             std::this_thread::sleep_for(1ms);
         }
     }
 
-    void get_float(const std::string &a_string, int a_beginning, float &float_value)
+    void get_float(const std::string& a_string, int a_beginning, float& float_value)
     {
-        
+
         std::string hex_value = a_string.substr(a_beginning, 8); // Extract hexadecimal value
-        std::cout << hex_value << std::endl<< std::flush;
-        uint32_t float_hex = strtoul(hex_value.c_str(), NULL, 16); // Convert hexadecimal string to unsigned long
-        //float_value = 0.f;
-        //uint32_t float_hex = std::stoul(hex_value, nullptr, 16); // Convert hexadecimal string to integer
-        float_value = *(float *)&float_hex;                       // Convert integer to float
+        std::cout << hex_value << std::endl << std::flush;
+        uint32_t float_hex
+          = strtoul(hex_value.c_str(), NULL, 16); // Convert hexadecimal string to unsigned long
+        // float_value = 0.f;
+        // uint32_t float_hex = std::stoul(hex_value, nullptr, 16); // Convert hexadecimal string to
+        // integer
+        float_value = *(float*)&float_hex; // Convert integer to float
     }
 
     std::string read_line_from_serial()
@@ -242,16 +264,18 @@ private:
 
         if (line.length() >= 2 + 8 * 5 + 2 && line.substr(0, 2) == "o:") // Ensure CRC is included
         {
-            //RCLCPP_WARN(this->get_logger(), "Received: %s", line.c_str());
-            
+            // RCLCPP_WARN(this->get_logger(), "Received: %s", line.c_str());
+
             // Extract CRC from the received line
-            std::string crc_str = line.substr(line.length() - 3, 2); // Assuming CRC is two bytes long
+            std::string crc_str
+              = line.substr(line.length() - 3, 2); // Assuming CRC is two bytes long
             uint16_t received_crc = 0;
             memcpy(&received_crc, crc_str.c_str(), sizeof(uint16_t));
 
             // Compute CRC for the received data
-            uint16_t computed_crc = calculate_crc(reinterpret_cast<const uint8_t*>(line.c_str()), line.length() - 3); // Excluding CRC bytes
-            
+            uint16_t computed_crc = calculate_crc(reinterpret_cast<const uint8_t*>(line.c_str()),
+                                                  line.length() - 3); // Excluding CRC bytes
+
             // Validate CRC
             if (received_crc == computed_crc)
             {
@@ -273,7 +297,8 @@ private:
             else
             {
                 RCLCPP_ERROR(this->get_logger(), "CRC Error! Received: %s", line.c_str());
-                RCLCPP_ERROR_STREAM(this->get_logger(), received_crc << " != " << computed_crc << std::endl);
+                RCLCPP_ERROR_STREAM(this->get_logger(),
+                                    received_crc << " != " << computed_crc << std::endl);
             }
         }
         else
@@ -293,23 +318,24 @@ private:
             encoders_pub_->publish(encoders_msg);
         }*/
         isReading = false;
-}
+    }
 
-rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_cmd_vel_;
-rclcpp::Subscription<krabi_msgs::msg::MotorsCmd>::SharedPtr subscription_motors_cmd_;
-rclcpp::Subscription<krabi_msgs::msg::MotorsParameters>::SharedPtr subscription_motors_parameters_;
-rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr subscription_enable_motor_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_cmd_vel_;
+    rclcpp::Subscription<krabi_msgs::msg::MotorsCmd>::SharedPtr subscription_motors_cmd_;
+    rclcpp::Subscription<krabi_msgs::msg::MotorsParameters>::SharedPtr
+      subscription_motors_parameters_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr subscription_enable_motor_;
 
-rclcpp::Publisher<krabi_msgs::msg::OdomLighter>::SharedPtr odom_pub_;
-rclcpp::Publisher<krabi_msgs::msg::Encoders>::SharedPtr encoders_pub_;
+    rclcpp::Publisher<krabi_msgs::msg::OdomLighter>::SharedPtr odom_pub_;
+    rclcpp::Publisher<krabi_msgs::msg::Encoders>::SharedPtr encoders_pub_;
 
-rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
-int serial_fd_;
-bool isReading = false;
+    int serial_fd_;
+    bool isReading = false;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<SimpleBrokerSTM32>());
